b0cdfaea6b5e5f5ba5393dc24e797328497adcb8
Jasim9085 <juahmed9085@gmail.com>
2025-11-26 12:18:36 +0530

    Update MainActivity.java

--- app/src/main/java/com/lumina/notes/MainActivity.java
+++ app/src/main/java/com/lumina/notes/MainActivity.java
@@ -3,6 +3,7 @@ package com.lumina.notes;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
